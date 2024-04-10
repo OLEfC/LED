185d67ff35a8681d076033b7ea5c6bc3de092f7c
OLEfC <94687864+OLEfC@users.noreply.github.com>
2024-04-10 20:59:39 +0300

    Add files via upload

---
+++ led32.ino
@@ -0,0 +1,13 @@
+const int ledPin = 32;
+
+void setup() {
+    pinMode(ledPin, OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+   digitalWrite(ledPin, HIGH);  // turn on the LED
+  // put your main code here, to run repeatedly:
+
+}
